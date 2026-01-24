5403df01b50718b254c97409cd2e2d56f16801e2
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-01-24 17:08:50 +0100

    Add menu screen layout and resources

---
+++ app/src/androidTest/java/com/dejitarunoseireinoapuri/saikorodojo/feature/menu/presentation/MenuScreenTest.kt
@@ -0,0 +1,35 @@
+package com.dejitarunoseireinoapuri.saikorodojo.feature.menu.presentation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.dejitarunoseireinoapuri.saikorodojo.R
+import com.dejitarunoseireinoapuri.saikorodojo.ui.theme.SaikoroDojoTheme
+import org.junit.Rule
+import org.junit.Test
+
+class MenuScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun menuScreenShowsTitleAndButtons() {
+        composeTestRule.setContent {
+            SaikoroDojoTheme {
+                MenuScreen(
+                    onPlayClick = {},
+                    onRulesClick = {}
+                )
+            }
+        }
+
+        val title = composeTestRule.activity.getString(R.string.game_title)
+        val play = composeTestRule.activity.getString(R.string.play)
+        val rules = composeTestRule.activity.getString(R.string.rules)
+
+        composeTestRule.onNodeWithText(title).assertIsDisplayed()
+        composeTestRule.onNodeWithText(play).assertIsDisplayed()
+        composeTestRule.onNodeWithText(rules).assertIsDisplayed()
+    }
+}
