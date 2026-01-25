930ee48af6a76e0ef72dc4ac87819724149ecd2e
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-01-25 15:45:58 +0100

    Added kover

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlinx.kover") version "0.9.4"
 }
 
 android {
