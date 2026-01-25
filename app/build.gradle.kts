f58d580911c985dc46bd96dd0c67093f3e47151a
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-01-25 15:52:20 +0100

    Add kover catalog alias and expand unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlinx.kover") version "0.9.4"
+    alias(libs.plugins.kotlinx.kover)
 }
 
 android {
