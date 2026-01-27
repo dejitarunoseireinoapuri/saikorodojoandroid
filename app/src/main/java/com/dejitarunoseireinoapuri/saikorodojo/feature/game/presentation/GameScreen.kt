e75827ffa7051af677a818f48b26e8400c473f83
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-01-27 11:35:16 +0100

    improved imports

--- app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/game/presentation/GameScreen.kt
+++ app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/game/presentation/GameScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
