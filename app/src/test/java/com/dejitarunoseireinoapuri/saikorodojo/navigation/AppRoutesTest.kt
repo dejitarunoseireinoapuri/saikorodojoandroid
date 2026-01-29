845063a15f8241fcae8be52e992110a7f004f0fb
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-01-29 16:43:22 +0100

    Use JUnit imports in unit tests

--- app/src/test/java/com/dejitarunoseireinoapuri/saikorodojo/navigation/AppRoutesTest.kt
+++ app/src/test/java/com/dejitarunoseireinoapuri/saikorodojo/navigation/AppRoutesTest.kt
@@ -1,7 +1,7 @@
 package com.dejitarunoseireinoapuri.saikorodojo.navigation
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class AppRoutesTest {
     @Test
