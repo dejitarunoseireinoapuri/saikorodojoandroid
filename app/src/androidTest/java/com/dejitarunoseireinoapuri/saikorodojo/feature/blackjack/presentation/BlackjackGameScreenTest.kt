9cc970f43f240de893dd48f1f9cbe49be849b548
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-02-02 14:46:54 +0100

    Update blackjack mats for win/loss

---
+++ app/src/androidTest/java/com/dejitarunoseireinoapuri/saikorodojo/feature/blackjack/presentation/BlackjackGameScreenTest.kt
@@ -0,0 +1,69 @@
+package com.dejitarunoseireinoapuri.saikorodojo.feature.blackjack.presentation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.graphics.toPixelMap
+import androidx.compose.ui.test.captureToImage
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.dejitarunoseireinoapuri.saikorodojo.feature.blackjack.domain.BlackjackOutcome
+import com.dejitarunoseireinoapuri.saikorodojo.ui.theme.FailureMatBackground
+import com.dejitarunoseireinoapuri.saikorodojo.ui.theme.SaikoroDojoTheme
+import com.dejitarunoseireinoapuri.saikorodojo.ui.theme.VictoryMatBackground
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class BlackjackGameScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun playerLossUsesFailureMatBackground() {
+        composeTestRule.setContent {
+            SaikoroDojoTheme {
+                BlackjackGameScreen(
+                    uiState = BlackjackGameUiState(
+                        isStarted = true,
+                        playerDice = listOf(2, 3),
+                        dealerDice = listOf(6),
+                        result = BlackjackOutcome.PLAYER_LOSE
+                    ),
+                    onStartClick = {},
+                    onHitClick = {},
+                    onStandClick = {},
+                    onContinueClick = {}
+                )
+            }
+        }
+
+        val image = composeTestRule.onNodeWithTag(BLACKJACK_PLAYER_MAT_TAG).captureToImage()
+        val pixelMap = image.toPixelMap()
+        val centerColor = pixelMap[pixelMap.width / 2, pixelMap.height / 2]
+        assertEquals(FailureMatBackground, centerColor)
+    }
+
+    @Test
+    fun playerWinUsesVictoryMatBackground() {
+        composeTestRule.setContent {
+            SaikoroDojoTheme {
+                BlackjackGameScreen(
+                    uiState = BlackjackGameUiState(
+                        isStarted = true,
+                        playerDice = listOf(4, 5),
+                        dealerDice = listOf(6),
+                        result = BlackjackOutcome.PLAYER_WIN
+                    ),
+                    onStartClick = {},
+                    onHitClick = {},
+                    onStandClick = {},
+                    onContinueClick = {}
+                )
+            }
+        }
+
+        val image = composeTestRule.onNodeWithTag(BLACKJACK_PLAYER_MAT_TAG).captureToImage()
+        val pixelMap = image.toPixelMap()
+        val centerColor = pixelMap[pixelMap.width / 2, pixelMap.height / 2]
+        assertEquals(VictoryMatBackground, centerColor)
+    }
+}
