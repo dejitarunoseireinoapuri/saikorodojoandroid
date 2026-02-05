b452a24a1270bad95e9d8f569948f81e87c3c80f
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-02-05 19:21:50 +0100

    Fixed import

--- app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/blackjack/presentation/BlackjackGameScreen.kt
+++ app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/blackjack/presentation/BlackjackGameScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
