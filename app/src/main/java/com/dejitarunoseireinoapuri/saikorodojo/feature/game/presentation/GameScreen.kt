05197b80635c975697006d119c3803f2163cbb26
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-02-09 11:48:28 +0100

    Fix objective info icon size import

--- app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/game/presentation/GameScreen.kt
+++ app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/game/presentation/GameScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.ui.draw.clipToBounds
