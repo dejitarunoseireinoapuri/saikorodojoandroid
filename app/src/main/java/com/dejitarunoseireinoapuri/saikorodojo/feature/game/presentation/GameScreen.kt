ee8f43b9678eb486fe7284181733f64dbd8e8f66
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-02-09 10:45:59 +0100

    Add missing heightIn import

--- app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/game/presentation/GameScreen.kt
+++ app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/game/presentation/GameScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
