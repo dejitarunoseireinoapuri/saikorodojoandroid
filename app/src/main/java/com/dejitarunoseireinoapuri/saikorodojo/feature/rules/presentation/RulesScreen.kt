8bf9634d764cb7d60742c71d4885bda826a1bb84
dejitarunoseireinoapuri <dejitarunoseireinoapuri@gmail.com>
2026-02-10 09:31:02 +0100

    Opt in to experimental Material3 API on rules screen

--- app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/rules/presentation/RulesScreen.kt
+++ app/src/main/java/com/dejitarunoseireinoapuri/saikorodojo/feature/rules/presentation/RulesScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -32,6 +33,7 @@ fun RulesRoute(
     RulesScreen(onNavigateBack = onNavigateBack)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RulesScreen(
     modifier: Modifier = Modifier,
